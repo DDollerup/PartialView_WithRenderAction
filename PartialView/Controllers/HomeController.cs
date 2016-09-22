73dee8a0d9a4af7e05a8fb59d4db4c913bff4211
PDDaniel <daniel@proper-dog.com>
2016-09-22 11:35:58 +0200

    Add project files.

---
+++ PartialView/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartialView.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<string> testList;
+
+        public HomeController()
+        {
+            testList = new List<string>()
+            {
+                "asd1",
+                "asd2",
+                "asd3",
+                "asd4",
+                "asd5"
+            };
+        }
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult TestList()
+        {
+            return PartialView(testList);
+        }
+    }
+}
